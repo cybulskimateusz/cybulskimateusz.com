d9e30092548a99b8a4901d832eaeb3c133d99e34
cybulskimateusz <cybulskimateusz@icloud.com>
2020-08-06 15:30:33 +0200

    setup

---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const { SourceMapDevToolPlugin } = require('webpack')
+
+module.exports = {
+  entry: './src/index.js',
+
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+
+  resolve: {
+    modules: [
+      path.resolve(__dirname, 'src'),
+      path.resolve(__dirname, 'node_modules')
+    ],
+    extensions: ['.js', '.jsx', '.json']
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.pug?$/,
+        use: ['pug-loader']
+      },
+      {
+        test: /\.js?$/,
+        exclude: [/node_modules/, /\.(test|spec).js?$/],
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.scss$/,
+        exclude: [/node_modules/],
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.(gltf|glsl|frag|vert)?$/,
+        loader: 'file-loader'
+      }
+    ]
+  },
+
+  devtool: 'source-map',
+
+  devServer: {
+    historyApiFallback: true,
+    public: 'localhost:8080',
+    host: '0.0.0.0',
+    hot: true
+  },
+
+  plugins: [
+    new HTMLWebpackPlugin({
+      filename: 'index.html',
+      template: path.join(__dirname, 'src/index.pug')
+    }),
+    new SourceMapDevToolPlugin({})
+  ]
+}
